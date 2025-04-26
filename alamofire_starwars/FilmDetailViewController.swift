41e6bbe13ce733e1c56f09f6cd985bea3ecb45de
zehragizem <zgduman@gmail.com>
2025-04-27 02:14:25 +0300

    FilmDetailViewController UI design completed.

--- alamofire_starwars/FilmDetailViewController.swift
+++ alamofire_starwars/FilmDetailViewController.swift
@@ -8,40 +8,72 @@
 
 import UIKit
 
-class FilmDetailViewController: UIViewController {
+class FilmDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var filmData: FilmData?
     
-
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var directorLabel: UILabel!
-    @IBOutlet weak var producerLabel: UILabel!
-    @IBOutlet weak var releaseDateLabel: UILabel!
-
-    @IBOutlet weak var starshipsTextView: UITextView!
-    @IBOutlet weak var vehiclesTextView: UITextView!
-    @IBOutlet weak var planetsTextView: UITextView!
-    @IBOutlet weak var charactersTextView: UITextView!
-    @IBOutlet weak var speciesTextView: UITextView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    enum SectionType: Int, CaseIterable {
+        case starships, vehicles, planets, characters, species
+        
+        var title: String {
+            switch self {
+            case .starships: return "Starships"
+            case .vehicles: return "Vehicles"
+            case .planets: return "Planets"
+            case .characters: return "Characters"
+            case .species: return "Species"
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        guard let film = filmData?.properties else { return }
-
-        titleLabel.text = film.title
-        directorLabel.text = film.director
-        producerLabel.text = film.producer
-        releaseDateLabel.text = film.releaseDate
-
-        // Örnek: starship isimlerini göster
-        starshipsTextView.text = filmData?.starships.map { $0.name }.joined(separator: "\n")
-        charactersTextView.text = filmData?.characters.map { $0.name }.joined(separator: "\n")
-        vehiclesTextView.text = filmData?.vehicles.map { $0.name }.joined(separator: "\n")
-        planetsTextView.text = filmData?.planets.map { $0.name }.joined(separator: "\n")
-        speciesTextView.text = filmData?.species.map { $0.name }.joined(separator: "\n")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DetailCell")
+        
+        title = filmData?.properties.title ?? "Film Detail"
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SectionType.allCases.count
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let film = filmData else { return 0 }
+        switch SectionType(rawValue: section)! {
+        case .starships: return film.starships.count
+        case .vehicles: return film.vehicles.count
+        case .planets: return film.planets.count
+        case .characters: return film.characters.count
+        case .species: return film.species.count
+        }
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return SectionType(rawValue: section)?.title
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+        guard let film = filmData else { return cell }
+
+        let section = SectionType(rawValue: indexPath.section)!
+        switch section {
+        case .starships:
+            cell.textLabel?.text = film.starships[indexPath.row].name
+        case .vehicles:
+            cell.textLabel?.text = film.vehicles[indexPath.row].name
+        case .planets:
+            cell.textLabel?.text = film.planets[indexPath.row].name
+        case .characters:
+            cell.textLabel?.text = film.characters[indexPath.row].name
+        case .species:
+            cell.textLabel?.text = film.species[indexPath.row].name
+        }
+
+        return cell
+    }
 }
-
-
